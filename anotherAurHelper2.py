2d0a6b60ef01c49385862006dece1b3521e94843
Stephen Seo <seo.disparate@gmail.com>
2025-11-11 14:28:56 +0900

    Begin work on helper.py and example config

---
+++ anotherAurHelper2.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import argparse
+import datetime
+import tomllib
+
+STRFTIME_LOCAL_FORMAT = "%Y-%m-%dT%H:%M:%S%:z"
+
+
+def timedelta_to_offset_string(timed: datetime.timedelta) -> str:
+    """Returns a timedelta string in the format "+HH:MM" or "-HH:MM"."""
+
+    seconds = timed.days * 24 * 60 * 60 + timed.seconds
+    minutes_offset = int(seconds / 60)
+    hours_offset = int(minutes_offset / 60)
+    minutes_offset = abs(minutes_offset - hours_offset * 60)
+    return f"{hours_offset:+03d}:{minutes_offset:02d}"
+
+
+def log_print(*args, **kwargs):
+    """Prints to stdout and logs the same to a log file."""
+    if "toml" in kwargs:
+        if "tz_force_offset_hours" in kwargs["toml"]:
+            offset_hours = kwargs["toml"]["tz_force_offset_hours"]
+            offset_minutes = 0
+            if "tz_force_offset_minutes" in kwargs["toml"]:
+                offset_minutes = kwargs["toml"]["tz_force_offset_minutes"]
+            tz = datetime.timezone(
+                datetime.timedelta(hours=offset_hours, minutes=offset_minutes)
+            )
+            lt = datetime.datetime.now(tz)
+            time_str = lt.strftime(STRFTIME_LOCAL_FORMAT)
+        else:
+            lt = datetime.datetime.now().astimezone()
+            time_str = lt.strftime(STRFTIME_LOCAL_FORMAT)
+        if "log_file" in kwargs["toml"]:
+            log_file = kwargs["toml"]["log_file"]
+        else:
+            log_file = "anotherAurHelper2.log"
+    else:
+        lt = datetime.datetime.now().astimezone()
+        time_str = lt.strftime(STRFTIME_LOCAL_FORMAT)
+        log_file = "anotherAurHelper2.log"
+    print(time_str, end=" ")
+    with open(log_file, "a", encoding="utf-8") as lf:
+        print(time_str, end=" ", file=lf)
+
+    if "toml" in kwargs:
+        del kwargs["toml"]
+
+    print(*args, **kwargs)
+    with open(log_file, "a", encoding="utf-8") as lf:
+        kwargs["file"] = lf
+        print(*args, **kwargs)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="AnotherAURHelper2",
+        description="Builds AUR pkgs in a chroot handled by systemd-nspawn",
+    )
+    parser.add_argument("-c", "--config")
+    args = parser.parse_args()
+
+    if args.config is None:
+        log_print("ERROR: config not specified!")
+        parser.print_usage()
+        return
+
+    toml_d = None
+    with open(args.config, "rb") as f:
+        toml_d = tomllib.load(f)
+    if toml_d is None:
+        log_print("ERROR: Failed to parse toml config file!")
+        parser.print_usage()
+        return
+    log_print("test", toml=toml_d)
+
+
+if __name__ == "__main__":
+    main()
